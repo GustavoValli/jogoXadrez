398a08db4de7ba88fc87d05b9d47bb90f4a718fb
GustavoValli <valligustavo2005@gmail.com>
2023-09-22 14:38:43 -0300

    Adicionando a classe ChessPosition e melhorando a classe ChessMatch

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -26,8 +26,17 @@ public class ChessMatch {
         return pieces;
     }
 
+    private void placeNewPiece(char column, int row, ChessPiece piece) {
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
+    }
+
     private void initialSetup() {
-        board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-        board.placePiece(new King(board, Color.WHITE), new Position(0, 0));
+        placeNewPiece('a', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('h', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 8, new King(board, Color.BLACK));
+
+        placeNewPiece('a', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('h', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 1, new King(board, Color.WHITE));
     }
 }
