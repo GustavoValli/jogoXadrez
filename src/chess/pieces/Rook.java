e1fe336bbbf73d4940b47ff0b14875a5a5895eb4
GustavoValli <valligustavo2005@gmail.com>
2023-09-26 16:08:02 -0300

    Movimentação da torre

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -18,6 +19,49 @@ public class Rook extends ChessPiece {
     @Override
     public boolean[][] possibleMoves() {
         boolean[][] possibleMoves = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        Position position = new Position(0, 0);
+
+        //up
+        position.setValues(this.position.getRow() - 1, this.position.getColumn());
+        while (getBoard().positionExists(position) && !getBoard().thereIsAPiece(position)) {
+            possibleMoves[position.getRow()][position.getColumn()] = true;
+            position.setRow(position.getRow() - 1);
+        }
+        if (getBoard().positionExists(position) && isThereOpponentPiece(position)) {
+            possibleMoves[position.getRow()][position.getColumn()] = true;
+        }
+
+        //left
+        position.setValues(this.position.getRow(), this.position.getColumn() - 1);
+        while (getBoard().positionExists(position) && !getBoard().thereIsAPiece(position)) {
+            possibleMoves[position.getRow()][position.getColumn()] = true;
+            position.setColumn(position.getColumn() - 1);
+        }
+        if (getBoard().positionExists(position) && isThereOpponentPiece(position)) {
+            possibleMoves[position.getRow()][position.getColumn()] = true;
+        }
+
+        //down
+        position.setValues(this.position.getRow() + 1, this.position.getColumn());
+        while (getBoard().positionExists(position) && !getBoard().thereIsAPiece(position)) {
+            possibleMoves[position.getRow()][position.getColumn()] = true;
+            position.setRow(position.getRow() + 1);
+        }
+        if (getBoard().positionExists(position) && isThereOpponentPiece(position)) {
+            possibleMoves[position.getRow()][position.getColumn()] = true;
+        }
+
+        //right
+        position.setValues(this.position.getRow(), this.position.getColumn() + 1);
+        while (getBoard().positionExists(position) && !getBoard().thereIsAPiece(position)) {
+            possibleMoves[position.getRow()][position.getColumn()] = true;
+            position.setColumn(position.getColumn() + 1);
+        }
+        if (getBoard().positionExists(position) && isThereOpponentPiece(position)) {
+            possibleMoves[position.getRow()][position.getColumn()] = true;
+        }
+
         return possibleMoves;
     }
 }
