7e01a0264fbf2d589cb3b5a64353208cb3ea15b1
GustavoValli <valligustavo2005@gmail.com>
2023-10-09 10:44:08 -0300

    Movimentos possíveis

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,11 +17,15 @@ public class Program {
 
         while (true) {
             try {
-                //UI.clearScreen();
+                UI.clearScreen();
                 UI.printBoard(chessMatch.getPieces());
                 System.out.println();
                 System.out.print("Source: ");
                 ChessPosition source = UI.readChessPosition(scanner);
+
+                boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces(), possibleMoves);
 
                 System.out.print("Target: ");
                 ChessPosition target = UI.readChessPosition(scanner);
