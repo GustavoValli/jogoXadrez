da3d1f9626834d74bcabeec305ebbbbf279aa015
GustavoValli <valligustavo2005@gmail.com>
2023-10-09 11:52:27 -0300

    Mudando as cores das peças

--- src/application/UI.java
+++ src/application/UI.java
@@ -13,23 +13,23 @@ public class UI {
     // https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
 
     public static final String ANSI_RESET = "\u001B[0m";
-    /* public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_BLACK = "\u001B[30m";
     public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_GREEN = "\u001B[32m"; */
+    public static final String ANSI_GREEN = "\u001B[32m";
     public static final String ANSI_YELLOW = "\u001B[33m";
-    /* public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_BLUE = "\u001B[34m";
     public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_CYAN = "\u001B[36m"; */
+    public static final String ANSI_CYAN = "\u001B[36m";
     public static final String ANSI_WHITE = "\u001B[37m";
 
-    /* public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+    public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
     public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
     public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
-    public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m"; */
+    public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
     public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
-    /* public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+    public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
-    public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m"; */
+    public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
     /*https://stackoverflow.com/questions/2979383/how-to-clear-the-console-using-java
     Not work in IDEs terminal*/
@@ -80,16 +80,16 @@ public class UI {
 
     private static void printPiece(ChessPiece piece, boolean background) {
         if (background) {
-            System.out.print(ANSI_BLUE_BACKGROUND);
+            System.out.print(ANSI_BLACK_BACKGROUND);
         }
         if (piece == null) {
             System.out.print("-" + ANSI_RESET);
         } else {
-            if (piece.getColor() == Color.WHITE) {
-                System.out.print(ANSI_WHITE + piece + ANSI_RESET);
+            if (piece.getColor() == Color.BLUE) {
+                System.out.print(ANSI_BLUE + piece + ANSI_RESET);
             }
             else {
-                System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
+                System.out.print(ANSI_RED + piece + ANSI_RESET);
             }
         }
         System.out.print(" ");
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -15,7 +15,7 @@ public class ChessMatch {
     public ChessMatch() {
         board = new Board(8, 8);
         turn = 1;
-        currentPlayer = Color.WHITE;
+        currentPlayer = Color.BLUE;
         initialSetup();
     }
 
@@ -82,7 +82,7 @@ public class ChessMatch {
 
     private void nextTurn() {
         turn++;
-        currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.WHITE;
+        currentPlayer = (currentPlayer == Color.BLUE) ? Color.RED : Color.BLUE;
     }
 
     private void placeNewPiece(char column, int row, ChessPiece piece) {
@@ -90,12 +90,12 @@ public class ChessMatch {
     }
 
     private void initialSetup() {
-        placeNewPiece('a', 8, new Rook(board, Color.BLACK));
-        placeNewPiece('h', 8, new Rook(board, Color.BLACK));
-        placeNewPiece('e', 8, new King(board, Color.BLACK));
+        placeNewPiece('a', 8, new Rook(board, Color.RED));
+        placeNewPiece('h', 8, new Rook(board, Color.RED));
+        placeNewPiece('e', 8, new King(board, Color.RED));
 
-        placeNewPiece('a', 1, new Rook(board, Color.WHITE));
-        placeNewPiece('h', 1, new Rook(board, Color.WHITE));
-        placeNewPiece('e', 1, new King(board, Color.WHITE));
+        placeNewPiece('a', 1, new Rook(board, Color.BLUE));
+        placeNewPiece('h', 1, new Rook(board, Color.BLUE));
+        placeNewPiece('e', 1, new King(board, Color.BLUE));
     }
 }
